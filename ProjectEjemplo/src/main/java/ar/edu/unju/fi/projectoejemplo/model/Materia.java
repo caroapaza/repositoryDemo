02049622e5c8ea37d25a919805c45149c2c275b5
caroapaza <apazacarolina@gmail.com>
2022-04-05 18:28:01 -0300

    nuevos métodos clase Materia

--- ProjectEjemplo/src/main/java/ar/edu/unju/fi/projectoejemplo/model/Materia.java
+++ ProjectEjemplo/src/main/java/ar/edu/unju/fi/projectoejemplo/model/Materia.java
@@ -15,6 +15,37 @@ public class Materia {
 		this.codigo = codigo;
 		this.curso = curso;
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	@Override
+	public String toString() {
+		return "Materia [nombre=" + nombre + ", codigo=" + codigo + ", curso=" + curso + "]";
+	}
+	
+	
 	
 	
 
