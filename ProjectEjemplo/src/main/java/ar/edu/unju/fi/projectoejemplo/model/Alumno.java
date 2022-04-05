4df25d159247f0755f631e240f57f91e91b3285b
caroapaza <apazacarolina@gmail.com>
2022-04-05 18:15:32 -0300

    métodos accesores

--- ProjectEjemplo/src/main/java/ar/edu/unju/fi/projectoejemplo/model/Alumno.java
+++ ProjectEjemplo/src/main/java/ar/edu/unju/fi/projectoejemplo/model/Alumno.java
@@ -15,6 +15,32 @@ public class Alumno {
 		this.libreta = libreta;
 		this.edad = edad;
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getLibreta() {
+		return libreta;
+	}
+
+	public void setLibreta(String libreta) {
+		this.libreta = libreta;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
 	
 	
 
