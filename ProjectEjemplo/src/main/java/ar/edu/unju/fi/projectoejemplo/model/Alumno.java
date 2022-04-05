9be80231825f58cb48327172dd6f4df1e5be7ca4
caroapaza <apazacarolina@gmail.com>
2022-04-05 18:12:47 -0300

    atributos iniciales y constructores para clase Alumno

---
+++ ProjectEjemplo/src/main/java/ar/edu/unju/fi/projectoejemplo/model/Alumno.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.projectoejemplo.model;
+
+public class Alumno {
+	private String nombre;
+	private String libreta;
+	private int edad;
+	
+	public Alumno() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Alumno(String nombre, String libreta, int edad) {
+		super();
+		this.nombre = nombre;
+		this.libreta = libreta;
+		this.edad = edad;
+	}
+	
+	
+
+}
