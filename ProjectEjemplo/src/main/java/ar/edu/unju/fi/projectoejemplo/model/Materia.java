0990d0e4248ba33d24c6878282c3a19b4fd2e57a
caroapaza <apazacarolina@gmail.com>
2022-04-05 18:24:33 -0300

    atributos y constructores de clase Materia

---
+++ ProjectEjemplo/src/main/java/ar/edu/unju/fi/projectoejemplo/model/Materia.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.projectoejemplo.model;
+
+public class Materia {
+	private String nombre;
+	private int codigo;
+	private String curso;
+	
+	public Materia() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Materia(String nombre, int codigo, String curso) {
+		super();
+		this.nombre = nombre;
+		this.codigo = codigo;
+		this.curso = curso;
+	}
+	
+	
+
+}
