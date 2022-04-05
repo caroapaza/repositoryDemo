5a1018194cd8063e7f90eda626aa3b3515b61306
caroapaza <apazacarolina@gmail.com>
2022-04-05 17:56:46 -0300

    primer commit

---
+++ ProjectEjemplo/src/main/java/ar/edu/unju/fi/projectoejemplo/ProjectEjemploApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.projectoejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEjemploApplication.class, args);
+	}
+
+}
